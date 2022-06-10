82b11a0d471e95bc1dd5b3571d2b4c929772d826
sharkwhale0313 <71440792+sharkwhale0313@users.noreply.github.com>
2022-06-10 17:38:11 +0900

    feat : Add 'createForm' method

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MemberController {
@@ -11,5 +12,10 @@ public class MemberController {
     //@Autowired // 스프링이 연관된 객체를 스프링 컨테이너에서 찾아서 넣어줌 (의존 관계를 외부에서 넣어주는 것) -> 의존성 주입 DI
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping(value = "/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
     }
 }
