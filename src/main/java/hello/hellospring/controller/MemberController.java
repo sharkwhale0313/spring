ab0290129cc13eeb792d6c4965c344be198184eb
sharkwhale0313 <71440792+sharkwhale0313@users.noreply.github.com>
2022-06-11 18:36:33 +0900

    feat : Add 'list' method (전체 회원 조회)

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -4,8 +4,11 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -32,5 +35,13 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/"; // home 화면으로 이동
+    }
+
+    // 전체 회원 조회
+    @GetMapping(value = "/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members); // member list를 모델에 담아 화면에 넘긴다
+        return "members/memberList";
     }
 }
